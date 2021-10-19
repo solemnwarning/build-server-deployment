06f0275e27b5379ed634de196876fb92d41e0bd8
Daniel Collins <solemnwarning@solemnwarning.net>
2021-10-19 11:55:46 +0100

    Replace master images that are more than 28 days old (#1).

--- .github/workflows/check-ami.sh
+++ .github/workflows/check-ami.sh
@@ -14,8 +14,11 @@ dir_sha=$(git log --pretty='format:%h' -1 -- "$1")
 echo "Directory $1 is at $dir_sha"
 echo ::set-output name=dir_sha::$dir_sha
 
-# If we're running on master, check that a master image exists. If on a topic branch, a build from
-# any branch with the same SHA will do, since its only for (manual) testing.
+# If we're running on master, check that a recent (within 28 days) master image
+# exists. If on a topic branch, a build from any branch with the same SHA will
+# do, since its only for (manual) testing.
+
+MAX_MASTER_AGE=$[ 60 * 60 * 24 * 28 ]
 
 if [ "$branch_name" = "master" ]
 then
@@ -24,11 +27,37 @@ else
 	ami_name_filter="$1-*-${dir_sha}-*"
 fi
 
-ami_count=$(aws ec2 describe-images --owners self --filters "Name=name,Values=${ami_name_filter}" | jq '.Images | length')
+ami_names=$(aws ec2 describe-images --owners self --filters "Name=name,Values=${ami_name_filter}" | jq -r '.Images[].Name')
+ami_count=$(wc -l <<< "${ami_names}")
+ami_ok=false
 
-echo "Found ${ami_count} AMIs matching expression '${ami_name_filter}'"
+echo "Found ${ami_count} AMIs matching expression '${ami_name_filter}':"
 
-if [ "$ami_count" -gt 0 ]
+for ami_name in "${ami_names}"
+do
+	# Extract date/time from AMI as "YYYYMMDD HH:MM:SS"
+	ami_date=$(sed -Ee 's/^.*-([0-9]{8})-([0-9]{2})([0-9]{2})([0-9]{2})$/\1 \2:\3:\4/' <<< "$ami_name")
+	
+	# ...convert to UNIX timestamp
+	ami_timestamp=$(TZ=UTC date --date="${ami_date}" +%s)
+	
+	# ...subtract from current time to find age
+	ami_age_secs=$[ $(date +%s) - ${ami_timestamp} ]
+	
+	echo "- ${ami_name} (built ${ami_age_secs} seconds ago)"
+	
+	if [ "$branch_name" = "master" ]
+	then
+		if [ "${ami_age_secs}" -le "${MAX_MASTER_AGE}" ]
+		then
+			ami_ok=true
+		fi
+	else
+		ami_ok=true
+	fi
+done
+
+if [ "$ami_ok" = "true" ]
 then
 	echo ::set-output name=build_ami::false
 else
